870ff60cce39f0087d7923cfc0a4390094510eff
mattmald <Mat.maldonado012@gmail.com>
2023-02-14 18:47:35 -0600

    started pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Auth from '../utils/auth';
 
+import Auth from '../utils/auth';
+import { useQuery } from '@apollo/client';
+
+
 return (
     <main> 
         <div>
--- server/server.js
+++ server/server.js
@@ -38,6 +38,6 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
 };
-  
+ 
 // Call the async function to start the server
 startApolloServer(typeDefs, resolvers);
