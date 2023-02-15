f0a051bba142345b6211af3fbbe36962d9256833
mattmald <Mat.maldonado012@gmail.com>
2023-02-14 20:45:33 -0600

    page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import Auth from '../utils/auth';
+import NavBar from '../components/NavBar'
 
-import Auth from '../utils/auth';
-import { useQuery } from '@apollo/client';
-
-
+const Home = () => {
 return (
-    <main> 
-        <div>
-        
-        </div>
-    </main> 
+    <div>
+        <h1>Welcome to Helping Hands!</h1>
+        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vitae odio a lacus placerat ultrices. Cras commodo et ante vitae auctor. Phasellus vulputate massa ac venenatis sollicitudin. Cras vel ligula purus. Cras eu nisl ipsum. Etiam a lorem massa. Morbi rutrum efficitur lacus, vitae volutpat urna placerat in. Duis ornare euismod cursus. Proin hendrerit tempor lectus, vitae imperdiet quam cursus ut. Vivamus sit amet fringilla mi. Nunc eu laoreet nisi.</p> 
+    </div> 
 );
+};
+
+export default Home; 
