12d66c6c5f234745645f92965799f0817452aa4e
Janelle <110637390+janellecikanek@users.noreply.github.com>
2023-02-18 11:29:16 -0600

    photo carousel

---
+++ client/src/pages/Updates.js
@@ -0,0 +1,41 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+import family from "../assets/family.jpeg"
+import family1 from "../assets/family1.jpeg"
+import finish from "../assets/finish.jpeg"
+import group from "../assets/group.jpeg"
+import kids from "../assets/kids.jpeg"
+import kids2 from "../assets/kids2.jpeg"
+
+const Updates = () => {
+        return (
+            <Carousel>
+                <div>
+                    <img src={family}/>
+                    <p className="photos">Legend 1</p>
+                </div>
+                <div>
+                <img src={family1}/>
+                    <p className="photos">Legend 2</p>
+                </div>
+                <div>
+                <img src={finish}/>
+                    <p className="photos">Legend 3</p>
+                </div>
+                <div>
+                    <img src={group}/>
+                    <p className="photos">Legend 1</p>
+                </div>
+                <div>
+                <img src={kids}/>
+                    <p className="photos">Legend 2</p>
+                </div>
+                <div>
+                <img src={kids2}/>
+                    <p className="photos">Legend 3</p>
+                </div>
+            </Carousel>
+        );
+    };
+
+export default Updates; 
