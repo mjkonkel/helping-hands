3647a710a3460bde2742c37447e712167c795ed4
Janelle <110637390+janellecikanek@users.noreply.github.com>
2023-02-18 20:00:29 -0600

    carousel formatting

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Description 
 
-This app is designed for friends and family to help a loved one going through a significant medical condition. It answers the question let me know if I can help. Designed to allow a family to ask and accept help without embarassment or creating additional burdens. 
+This app is designed for friends and family to help a loved one going through a significant medical condition. It answers the question let me know if I can help. Designed to allow a family to ask and accept help without embarrassment or creating additional burdens. 
 
 Meals are a great way to help someone in this situation. The difficulty for the family needing help is getting too much food at one time or foods family members do not like. The first section of the application will display dates a meal would be appreciated and meal ideas to decrease waste by sending food items which may not be useful due to restrictions, allergies or preferences. This page also provides useful tips such as requesting small serving sizes and donating fresh fruit and produce so the family does not need to go to the grocery store as often. 
 
-The secion section of the application will offer ideas for friends and family to provider other ways to help. This section would include items such as good playdate time for any children in the home, caring for animals, yardwork or housework. 
+The activity section of the application will offer ideas for friends and family to provider other ways to help. This section would include items such as good playdate time for any children in the home, caring for animals, yardwork or housework. 
 
 The third section provides an area for the family to post medical updates while allowing friends to post comments. There is also a photo carousel so those who may not be able be there physically may still feel close to their loved one who is ill.
 
--- client/src/pages/Updates.js
+++ client/src/pages/Updates.js
@@ -1,4 +1,4 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import family from "../assets/family.jpeg"
 import family1 from "../assets/family1.jpeg"
@@ -6,36 +6,39 @@ import finish from "../assets/finish.jpeg"
 import group from "../assets/group.jpeg"
 import kids from "../assets/kids.jpeg"
 import kids2 from "../assets/kids2.jpeg"
+import "../index.css"
 
 const Updates = () => {
-        return (
-            <Carousel>
-                <div>
-                    <img src={family}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                <img src={family1}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                <img src={finish}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                    <img src={group}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                <img src={kids}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                <img src={kids2}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-            </Carousel>
-        );
-    };
+    return (
+        <Carousel>
+
+            <div className="carousel">
+                <img src={kids} alt="kids at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+            <div className="carousel">
+                <img src={kids2} alt="kids at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+            <div className="carousel">
+                <img src={family} alt="family at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+            <div className="carousel">
+                <img src={family1} alt="family at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+            <div className="carousel">
+                <img src={finish} alt="finish line at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+            <div className="carousel">
+                <img src={group} alt="group at walk for the cure" className="photos" />
+                <p className="caption">Komen Minnesota Race for the Cure</p>
+            </div>
+
+        </Carousel>
+    );
+};
 
 export default Updates; 
