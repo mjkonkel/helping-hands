e09adf8c4c409d4ca637d0ebc0b9e84706fed26c
Janelle <110637390+janellecikanek@users.noreply.github.com>
2023-02-20 20:42:46 -0600

    text update to updates page

--- client/src/pages/Updates.js
+++ client/src/pages/Updates.js
@@ -1,4 +1,4 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import family from "../assets/family.jpeg"
 import family1 from "../assets/family1.jpeg"
@@ -8,41 +8,46 @@ import kids from "../assets/kids.jpeg"
 import kids2 from "../assets/kids2.jpeg"
 
 const Updates = () => {
-        return (
+    return (
 
+        <div>
             <div>
-                <div>
-                    <h1>Updatescle</h1>
-                </div>
-           
+                <h1>Updates</h1>
+                <h3>Race for the Cure</h3>
+                <p>
+                    We participated in the Walk for the Cure on Sunday. It is an amazing event, the kids had a lot of fun. My outpatient surgery has been scheduled for Tuesday with radiation to follow next month. Thank you for all of your support it means so much for all of us.
+                </p>
+            </div>
+
             <Carousel>
                 <div>
-                    <img src={family}/>
+                    <img src={finish} />
+                    <p className="photos">Komen Minnesota Race for the Cure</p>
+                </div>
+
+                <div>
+                    <img src={kids} />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                <img src={family1}/>
+                    <img src={kids2} />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                <img src={finish}/>
+                    <img src={group} />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                    <img src={group}/>
+                    <img src={family} />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                <img src={kids}/>
-                    <p className="photos">Komen Minnesota Race for the Cure</p>
-                </div>
-                <div>
-                <img src={kids2}/>
+                    <img src={family1} />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
             </Carousel>
-            </div>
-        );
-    };
+        </div>
+    );
+};
 
 export default Updates; 
