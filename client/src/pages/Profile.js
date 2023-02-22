e0df9b62ddb6c2da9f548d29d1d1ca7599bbe0c7
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-02-22 17:46:03 -0600

    got rid of add friend functionality

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,55 +8,17 @@ import Logo from "../assets/Logo.jpeg";
 
 const Profile = (props) => {
     const [username, setUsername] = useState(Auth.getProfile().data.username)
-    // const { username: userParam } = useParams();
-    // console.log(username)
 
-    const [addFriend] = useMutation(ADD_FRIEND);
-    // const { loading, data } = useQuery(username ? QUERY_USER : QUERY_ME, {
-    //     variables: { username },
-    // });
-    const { loading, data } = useQuery(QUERY_USER, {
-        variables: { username: "Molly48" }
-    })
-    // console.log("data", data)
-    // const user = data?.me || data?.user || {};
-    // console.log("user", user)
-    // console.log("login", Auth.loggedIn());
-    // console.log("getprofile", Auth.getProfile());
-    // navigate to personal profile page if username is yours
-    //  console.log("userParam", userParam)
-    // if (Auth.loggedIn()) {
-    //       return <Navigate to="/profile" />;
-    //  }
 
-    // if (loading) {
-    //     return <div>Loading...</div>;
-    // }
-
-    // if (!user?.username) {
-    //     return (
-    //         <div>
-    //             <div className="Logo">
-    //                 <img src={Logo} alt="Logo" />
-
-    //             </div>
-    //             <h4>
-    //                 You need to be logged in to see this. Use the navigation links above to
-    //                 sign up or log in!
-    //             </h4>
-    //         </div>
-    //     );
-    // }
-
-    const handleClick = async () => {
-        try {
-            // await addFriend({
-            //     variables: { id: user._id },
-            // });
-        } catch (e) {
-            console.error(e);
-        }
-    };
+    // const handleClick = async () => {
+    //     try {
+    //         // await addFriend({
+    //         //     variables: { id: user._id },
+    //         // });
+    //     } catch (e) {
+    //         console.error(e);
+    //     }
+    // };
 
     return (
         <div>
@@ -69,11 +31,11 @@ const Profile = (props) => {
                     Viewing {username}'s profile.
                 </h2>
 
-                {useParams && (
+                {/* {useParams && (
                     <button className="btn ml-auto" onClick={handleClick}>
                         Add Friend
                     </button>
-                )}
+                )} */}
             </div>
             <div>Hello {username}</div>
         </div>
