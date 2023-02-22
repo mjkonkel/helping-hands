22f316c41065fb1e726a1724e27280e8989baa56
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-02-22 17:33:01 -0600

    changed userParam to useParams

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -7,23 +7,23 @@ import Auth from '../utils/auth';
 import Logo from "../assets/Logo.jpeg";
 
 const Profile = (props) => {
-    const [username, setUsername] = useState(Auth.getProfile().data.username) 
-   // const { username: userParam } = useParams();
+    const [username, setUsername] = useState(Auth.getProfile().data.username)
+    // const { username: userParam } = useParams();
 
     const [addFriend] = useMutation(ADD_FRIEND);
     // const { loading, data } = useQuery(username ? QUERY_USER : QUERY_ME, {
     //     variables: { username },
     // });
-    const {loading, data}=useQuery(QUERY_USER, {
-        variables:{username:"Molly48"}
+    const { loading, data } = useQuery(QUERY_USER, {
+        variables: { username: "Molly48" }
     })
-console.log("data", data)
+    console.log("data", data)
     const user = data?.me || data?.user || {};
-     console.log("user", user)
+    console.log("user", user)
     console.log("login", Auth.loggedIn());
     console.log("getprofile", Auth.getProfile());
     // navigate to personal profile page if username is yours
-  //  console.log("userParam", userParam)
+    //  console.log("userParam", userParam)
     // if (Auth.loggedIn()) {
     //       return <Navigate to="/profile" />;
     //  }
@@ -47,17 +47,6 @@ console.log("data", data)
     //     );
     // }
 
-<<<<<<< HEAD
-    // const handleClick = async () => {
-    //     try {
-    //         await addFriend({
-    //             variables: { id: user._id },
-    //         });
-    //     } catch (e) {
-    //         console.error(e);
-    //     }
-    // };
-=======
     const handleClick = async () => {
         try {
             // await addFriend({
@@ -67,7 +56,6 @@ console.log("data", data)
             console.error(e);
         }
     };
->>>>>>> c314b54ce3bca07873285d342ac732f3257367e4
 
     return (
         <div>
@@ -76,15 +64,15 @@ console.log("data", data)
             </div>
 
             <div className="flex-row mb-3">
-                {/* <h2 className="bg-dark text-secondary p-3 display-inline-block">
-                    Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-                </h2> */}
+                <h2 className="bg-dark text-secondary p-3 display-inline-block">
+                    Viewing {useParams ? `${user.username}'s` : 'your'} profile.
+                </h2>
 
-                {/* {userParam && (
+                {useParams && (
                     <button className="btn ml-auto" onClick={handleClick}>
                         Add Friend
                     </button>
-                )} */}
+                )}
             </div>
             <div>hello</div>
         </div>
