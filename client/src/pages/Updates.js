95f372c7e19141d986e300e3954ce204f289351e
mattmald <Mat.maldonado012@gmail.com>
2023-02-21 22:27:29 -0600

    request are popping needs CSS

--- client/src/pages/Updates.js
+++ client/src/pages/Updates.js
@@ -2,7 +2,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import family from "../assets/family.jpeg"
 import family1 from "../assets/family1.jpeg"
-import finish from "../assets/finish.jpeg"
+// import finish from "../assets/finish.jpeg"
 import group from "../assets/group.jpeg"
 import kids from "../assets/kids.jpeg"
 import kids2 from "../assets/kids2.jpeg"
@@ -25,28 +25,28 @@ const Updates = () => {
 
                 
                 <h1 className="photos">Komen Minnesota Race for the Cure</h1>
-                    <img src={family} />
+                    <img src={family} alt=""/>
                     {/* <h1 className="photos">Komen Minnesota Race for the Cure</h1> */}
                 </div>
 
                 <div>
-                    <img src={kids} />
+                    <img src={kids} alt="" />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                    <img src={kids2} />
+                    <img src={kids2}  alt=""/>
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                    <img src={group} />
+                    <img src={group} alt="" />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                    <img src={family} />
+                    <img src={family} alt="" />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
                 <div>
-                    <img src={family1} />
+                    <img src={family1} alt="" />
                     <p className="photos">Komen Minnesota Race for the Cure</p>
                 </div>
             </Carousel>
