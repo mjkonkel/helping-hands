90f98cca353498533de38d0789cbf14b8e6a578e
Janelle <110637390+janellecikanek@users.noreply.github.com>
2023-02-21 19:57:58 -0600

    homepage logo

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,22 @@
 import React from 'react';
 // import NavBar from '../components/NavBar'
+import Logo from "../assets/Logo.jpeg";
+import Header from "../components/Header"
+
+console.log(Logo);
 
 const Home = () => {
-return (
+    return (
+        <div>
+        <div className="Logo">
+        <img src={Logo} alt="Logo" />
+     </div>
     <div>
         <h1>Welcome to Helping Hands!</h1>
         <p>Thank you for visiting Helping Hands where many hands make the workload smaller. Helping Hands is designed so you may provide the individualized help your loved one needs during this time.</p> 
-    </div> 
-);
+    </div>
+    </div>
+    );
 };
 
 export default Home; 
