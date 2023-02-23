40a44606b59be6baa532994072398f04d142243a
sophiekedrowski <105030180+sophiekedrowski@users.noreply.github.com>
2023-02-22 19:11:40 -0600

    Fixed profile

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/client';
-import { QUERY_USER, QUERY_ME, QUERY_REQUESTS } from '../utils/queries';
+import { QUERY_USER, QUERY_ME, QUERY_REQUESTS, QUERY_ALL_USERS } from '../utils/queries';
 import { ADD_FRIEND } from '../utils/mutations';
 import Auth from '../utils/auth';
 import Logo from "../assets/Logo.jpeg";
+import RequestList from '../components/Request';
 
 const Profile = (props) => {
     const { username: userParam } = useParams();
-
+    console.log(userParam)
     const [username, setUsername] = useState(Auth.getProfile().data.username)
 
-    const { loading, error, data } = useQuery( userParam ? QUERY_USER : QUERY_ME, {
-        variables: { username: userParam },
-      });
+    const { loading, error, data } = useQuery( QUERY_USER,{variables: {username:"test1"}});
 
     const user = data?.me || data?.user || {};
 
-    console.log(username)
-    console.log(data)
+    // console.log(username)
+    console.log(user)
 
     // const handleClick = async () => {
     //     try {
@@ -50,14 +49,13 @@ const Profile = (props) => {
                 <h2 className="bg-dark text-secondary p-3 display-inline-block">
                     Viewing {username}'s profile.
                 </h2>
-
-                {/* {useParams && (
-                    <button className="btn ml-auto" onClick={handleClick}>
-                        Add Friend
-                    </button>
-                )} */}
+                <div className="col-12 col-lg-3 mb-3">
+          <RequestList
+            requests = {user.requests}
+          />  
             </div>
             <div>Welcome {username}</div>
+        </div>
         </div>
     );
 };
